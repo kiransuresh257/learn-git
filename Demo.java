091add764709260f7c71b60431f4f56773673e9a
kiranshuttler257 <kiranshuttler2005@gmail.com>
2026-07-01 17:36:48 +0530

    demo program

---
+++ Demo.java
@@ -0,0 +1,21 @@
+public class Demo {
+    public static void main(String[] args) {
+        A objA = new A();
+        B objB = new B();
+        
+        objA.message();
+        objB.message();
+    }
+}
+
+class A {
+    void message() {
+        System.out.println("This is class A");
+    }
+}
+
+class B {
+    void message() {
+        System.out.println("This is class B");
+    }
+}
