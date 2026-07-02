170f722c624f0e713a1bdf9786e23afc7a8927a7
kiranshuttler257 <kiranshuttler2005@gmail.com>
2026-07-02 11:52:35 +0530

    s added

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Hello{
     public static void main(String[] args){
     System.out.print("Helloworld");
-     System.out.print("Hello kiran s");
+     System.out.print("Hello kiran ss");
 
 }
 }
